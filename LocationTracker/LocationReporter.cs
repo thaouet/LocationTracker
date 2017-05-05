2f1344db94552633a7db7aea7f5024a6db0f6857
thaouet <thaouet@outlook.com>
2017-05-05 06:29:33 +0100

    Ajoutez des fichiers projet.

---
+++ LocationTracker/LocationReporter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationTracker
+{
+    class LocationReporter : IObserver<Location>
+    {
+        private IDisposable unsubscriber;
+        private string instName;
+
+        public LocationReporter(string name)
+        {
+            this.instName = name;
+        }
+
+        public string Name
+        { get { return this.instName; } }
+
+        public virtual void Subscribe(IObservable<Location> provider)
+        {
+            if (provider != null)
+                unsubscriber = provider.Subscribe(this);
+        }
+
+        public virtual void OnCompleted()
+        {
+            Console.WriteLine("Le Location Tracker a complété la transmission de données à {0}.", this.Name);
+            this.Unsubscribe();
+        }
+
+        public virtual void OnError(Exception e)
+        {
+            Console.WriteLine("{0}: L’emplacement ne peut être déterminé.", this.Name);
+        }
+
+        public virtual void OnNext(Location value)
+        {
+            Console.WriteLine("{2}: L’emplacement actuel est {0}, {1}", value.Latitude, value.Longitude, this.Name);
+        }
+
+        public virtual void Unsubscribe()
+        {
+            unsubscriber.Dispose();
+        }
+
+    }
+}
---
+++ LocationTracker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocationTracker provider = new LocationTracker();
+            LocationReporter reporter1 = new LocationReporter("Fixed GPS");
+            reporter1.Subscribe(provider);
+            LocationReporter reporter2 = new LocationReporter("Mobile GPS");
+            reporter2.Subscribe(provider);
+
+            provider.TrackLocation(new Location(47.6456, -122.1312));
+            reporter1.Unsubscribe();
+            provider.TrackLocation(new Location(47.6677, -122.1199));
+            provider.TrackLocation(null);
+            provider.EndTransmission();
+            Console.ReadLine();
+        }
+    }
+}
